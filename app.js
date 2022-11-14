00d6e43a11bdb12d7eb9de0a8e976c99be31dce5
NikaMzhavanadze87 <n.m.mzhavanadze@gmail.com>
2022-11-14 22:33:23 +0400

    final

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ function validatePersonalNumber() {
 	if (personalNumber.value.length !==11) {
         small.innerText = "number must be 11 char or more";
         small.classList.add("error");
+        personalNumber.classList.add("error-input");
         personalNumberIsValid = false;
 
 	} 
    
     else{
         small.classList.remove("error");
+        personalNumber.classList.remove("error-input");
+        personalNumber.classList.add("success-input");
         personalNumberIsValid = true;
     }
     return personalNumberIsValid;
@@ -24,11 +27,14 @@ function validatePhoneNumber() {
 	if (mobileNumber.value.length !==9) {
         small.innerText = "number must be 9";
         small.classList.add("error");
+        mobileNumber.classList.add("error-input");
         mobilelNumberIsValid = false;
 
 	} 
     else{
         small.classList.remove("error");
+        mobileNumber.classList.remove("error-input");
+        mobileNumber.classList.add("success-input");
         mobilelNumberIsValid = true;
     }
     return mobilelNumberIsValid;
@@ -37,11 +43,14 @@ function validateJobDescription() {
 	if (jobDescription.value.length < 50) {
         small.innerText = "max char must be 50";
         small.classList.add("error");
+        jobDescription.classList.add("error-input");
         jobDescriptionIsValid = false;
 
 	} 
     else if(jobDescription.value.length == 50){
         small.classList.remove("error");
+        jobDescription.classList.remove("error-input");
+        jobDescription.classList.add("success-input");
         jobDescriptionIsValid = true;
     }
     return jobDescriptionIsValid;
